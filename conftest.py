699663ec82cfd0af4403764d15ba17574f345bed
kazimirez <kazimirpwnz@gmail.com>
2022-03-30 19:50:24 +0300

    recoded conftest for my taste, no more code from strangers i can't fully undestand

--- conftest.py
+++ conftest.py
@@ -2,12 +2,6 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-supported_browsers = {
-    'chrome': webdriver.Chrome,
-    'firefox': webdriver.Firefox
-}
-
-
 def pytest_collection_modifyitems(items, config):
     for item in items:
         if not any(item.iter_markers()):
@@ -26,14 +20,20 @@ def browser(request):
     browser_name = request.config.getoption("browser_name")
     browser_language = request.config.getoption("browser_language")
 
-    if browser_name in supported_browsers:
-        browser = supported_browsers.get(browser_name, browser_language)()
-        print(f"\nstart {browser_name} browser for test..")
+    if browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': browser_language})
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+
+    elif browser_name == "firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", browser_language)
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
-        joined_browsers = ', '.join(supported_browsers.keys())
-        raise pytest.UsageError(
-            f"--browser_name is invalid, supported browsers: {joined_browsers}")
-    browser.implicitly_wait(1)
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    browser.implicitly_wait(3)
     yield browser
     print("\nquit browser..")
     browser.quit()
--- test_data.py
+++ test_data.py
@@ -10,3 +10,4 @@ class TestLinks:
     main_page_link = 'http://demowebshop.tricentis.com'
     register_link = 'http://demowebshop.tricentis.com/register'
     account_page = 'http://demowebshop.tricentis.com/customer/changepassword'
+
